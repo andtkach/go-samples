aaeb89ab2f0a2a0b64497bbf809fad2f81bbe85b
Andrii Tkach <andtkach@gmail.com>
2022-02-02 19:53:52 +0200

    Added build all

---
+++ go-dist-app/app/build-all.cmd
@@ -0,0 +1,4 @@
+go build -o LogService.exe cmd\logservice\main.go
+go build -o GradingService.exe cmd\gradingservice\main.go
+go build -o RegistryService.exe cmd\registryservice\main.go
+go build -o TeacherPortal.exe cmd\teacherportal\main.go
